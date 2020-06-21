c029d567c580250ed457047995389ec89c4bdd77
Volodymyr Krekhovetskyi <v.krekhovetskyi@gmail.com>
2020-06-21 07:09:31 +0300

    app states implementation

---
+++ src/app/StateMediator.ts
@@ -0,0 +1,74 @@
+export enum StateType {
+  MainPanelOnly,
+  MainPanelWithSideNav,
+  DetailPanel
+}
+
+export enum PanelType {
+  Primary,
+  Detail
+}
+
+export interface IState {
+  getPanelType(): PanelType;
+
+  getStateType(): StateType;
+
+  isSideNavVisible(): boolean;
+
+  getPanelButtonClass(): string;
+}
+
+export class MainPanelOnly implements IState {
+  getPanelType(): PanelType {
+    return PanelType.Primary;
+  }
+
+  getStateType(): StateType {
+    return StateType.MainPanelOnly;
+  }
+
+  getPanelButtonClass(): string {
+    return 'fa-chevron-right';
+  }
+
+  isSideNavVisible(): boolean {
+    return false;
+  }
+}
+
+export class MainPanelWithSideNav implements IState {
+  getPanelType(): PanelType {
+    return PanelType.Primary;
+  }
+
+  getStateType(): StateType {
+    return StateType.MainPanelWithSideNav;
+  }
+
+  getPanelButtonClass(): string {
+    return 'fa-chevron-left';
+  }
+
+  isSideNavVisible(): boolean {
+    return true;
+  }
+}
+
+export class DetailPanel implements IState {
+  getPanelType(): PanelType {
+    return PanelType.Detail;
+  }
+
+  getStateType(): StateType {
+    return StateType.DetailPanel;
+  }
+
+  getPanelButtonClass(): string {
+    return '';
+  }
+
+  isSideNavVisible(): boolean {
+    return false;
+  }
+}
